16d57d6ae60690d73b8d00150c576699d7ec36ac
Douglas Alves Domingues <kamikai@gmail.com>
2023-03-28 00:13:53 -0300

    add showatms command for making it easier to search for atm locations

--- client/commands.lua
+++ client/commands.lua
@@ -1,3 +1,21 @@
+Commands = {}
+
+function Commands.showatms()
+  local objectPool = GetGamePool('CObject')
+  for _, entity in pairs(objectPool) do
+    local model = GetEntityModel(entity)
+    if (model == GetHashKey('prop_atm_01') or model == GetHashKey('prop_atm_02') or
+        model == GetHashKey('prop_atm_03') or model == GetHashKey('prop_fleeca_atm')) then
+      local coords = GetEntityCoords(entity, false)
+      local heading = GetEntityHeading(entity)
+      print(json.encode({ coords = coords, heading = heading }))
+    end
+  end
+end
 RegisterCommand('dinheiro', function(args)
   print(json.encode(args))
+end, true)
+
+RegisterCommand('showatms', function()
+  Commands.showatms()
 end, true)
