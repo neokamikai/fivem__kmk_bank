2986e030557da94406797c93edef192c9acf12bc
Douglas Alves Domingues <kamikai@gmail.com>
2023-03-28 00:12:50 -0300

    remove shapeTest logic + add more atm locations + add support for hiding atm icon on map

--- client/blips.lua
+++ client/blips.lua
@@ -8,25 +8,16 @@ local ATMHashes = {
   prop_atm_03 = { sint = -1364697528, usint = 2930269768 },
   prop_fleeca_atm = { sint = 506770882, usint = 506770882 }
 }
-if(Config)then
-  print ('Config available')
-  if (Config.InteractKeyMap) then
-    print('Config.InteractKeyMap available')
-  else
-    print('Config.InteractKeyMap not available')
-  end
-else
-  print ('Config not available')
-end
 local blips = { -- Example {title="", colour=, id=, x=, y=, z=},
   {
     title = "Caixa Eletrônico", -- Loja
     colour = 25,
     id = 207, 
-    x = 33.435,
-    y = -1347.314,
+    x = 33.249,
+    y = -1348.0,
     z = 29.500,
-    interactVec = { distance = 0.8, heading = 185, headingDelta = 7.8 },
+    -- setcoords 33.249 -1348.0 29.7
+    interactVec = { distance = 0.85, heading = 185, headingDelta = 7.8, view = false },
     modelHash = -870868698,
     marker = {
       x = 33.2,
@@ -44,7 +35,8 @@ local blips = { -- Example {title="", colour=, id=, x=, y=, z=},
     x = -721.168,
     y = -415.567,
     z = 34.0,
-    interactVec = { distance = 0.8, heading = 267, headingDelta = 15 },
+    -- setcoords -721.168 -415.567 34.2
+    interactVec = { distance = 1.2, heading = 267, headingDelta = 15, view = false },
     marker = {
       x = -720.768,
       y = -415.567,
@@ -61,12 +53,160 @@ local blips = { -- Example {title="", colour=, id=, x=, y=, z=},
     x = -717.347,
     y = -915.647,
     z = 19.216,
-    interactVec = { distance = 1.6, heading = 92, headingDelta = 7.8 },
+    -- setcoords -717.347 -915.647 21.0
+    interactVec = { distance = 1.6, heading = 95, headingDelta = 12, view = false },
     marker = {
       x = -718.247,
       y = -915.647,
       z = 20.216,
       scale = 0.75,
+      bopUpAndDown = true,
+      color = { r = 0, g = 170, b = 0, a = 192 }
+    },
+  },
+  {
+    title = "Caixa Eletrônico", -- Rua
+    colour = 25,
+    id = 207,
+    x = -821.8935546875,
+    y = -1081.5545654296876,
+    z = 10.1366,
+    -- setcoords -721.168 -415.567 34.2
+    interactVec = { distance = 1.2, heading = 30, headingDelta = 15, view = false },
+    marker = {
+      x = -821.8935546875,
+      y = -1081.5545654296876,
+      z = 10.1366 + 2,
+      scale = 0.5,
+      bopUpAndDown = true,
+      color = { r = 0, g = 170, b = 0, a = 192 }
+    },
+  },
+  {
+    title = "Caixa Eletrônico", -- Rua
+    colour = 25,
+    id = 207,
+    x = -526.779113,
+    y = -1223.3736572,
+    z = 17.4527206,
+    -- setcoords -721.168 -415.567 34.2
+    interactVec = { distance = 1.2, heading = 155, headingDelta = 15, view = false },
+    marker = {
+      x = -821.8935546875,
+      y = -1081.5545654296876,
+      z = 17.4527206 + 2,
+      scale = 0.5,
+      bopUpAndDown = true,
+      color = { r = 0, g = 170, b = 0, a = 192 }
+    },
+  },
+  {
+    title = "Caixa Eletrônico", -- Rua
+    colour = 25,
+    id = 207,
+    x = -537.805175,
+    y = -854.9356689,
+    z = 28.275428,
+    -- setcoords -721.168 -415.567 34.2
+    interactVec = { distance = 1.2, heading = 180, headingDelta = 15, view = false },
+    marker = {
+      x = -537.805175,
+      y = -854.9356689,
+      z = 28.275428 + 2,
+      scale = 0.5,
+      bopUpAndDown = true,
+      color = { r = 0, g = 170, b = 0, a = 192 }
+    },
+  },
+  --
+  {
+    title = "Caixa Eletrônico", -- Rua
+    colour = 25,
+    id = 207,
+    x = -712.9356689,
+    y = -818.48272705,
+    z = 22.740657806,
+    hidden = true,
+    -- setcoords -721.168 -415.567 34.2
+    interactVec = { distance = 1.2, heading = 359, headingDelta = 15, view = false },
+    marker = {
+      x = -712.9356689,
+      y = -818.48272705,
+      z = 22.740657806 + 2,
+      scale = 0.5,
+      bopUpAndDown = true,
+      color = { r = 0, g = 170, b = 0, a = 192 }
+    },
+  },
+  {
+    title = "Caixa Eletrônico", -- Rua
+    colour = 25,
+    id = 207,
+    x = -710.0827636,
+    y = -818.475585,
+    z = 22.736335,
+    -- setcoords -721.168 -415.567 34.2
+    interactVec = { distance = 1.2, heading = 359, headingDelta = 15, view = false },
+    marker = {
+      x = -710.0827636,
+      y = -818.475585,
+      z = 22.736335 + 2,
+      bopUpAndDown = true,
+      color = { r = 0, g = 170, b = 0, a = 192 }
+    },
+  },
+  {
+    title = "Caixa Eletrônico", -- Rua
+    colour = 25,
+    id = 207,
+    x = -660.676330,
+    y = -854.488159,
+    z = 23.456634,
+    -- setcoords -721.168 -415.567 34.2
+    interactVec = { distance = 1.2, heading = 180, headingDelta = 15, view = false },
+    marker = {
+      x = -660.676330,
+      y = -854.488159,
+      z = 23.456634 + 2,
+      scale = 0.5,
+      bopUpAndDown = true,
+      color = { r = 0, g = 170, b = 0, a = 192 }
+    },
+  },
+  --
+  {
+    title = "Caixa Eletrônico", -- Rua
+    colour = 25,
+    id = 207,
+    x = -1289.74194335,
+    y = -227.1649780,
+    z = 41.430309,
+    -- setcoords -721.168 -415.567 34.2
+    interactVec = { distance = 1.2, heading = 125, headingDelta = 15, view = false },
+    marker = {
+      x = -1289.74194335,
+      y = -227.1649780,
+      z = 41.430309 + 2,
+      scale = 0.5,
+      bopUpAndDown = true,
+      color = { r = 0, g = 170, b = 0, a = 192 }
+    },
+  },
+  {
+    title = "Caixa Eletrônico", -- Rua
+    colour = 25,
+    id = 207,
+    x = -1285.1364746,
+    y = -223.9421539,
+    z = 41.430309,
+    hidden = true,
+    -- setcoords -721.168 -415.567 34.2
+    interactVec = { distance = 1.2, heading = 305, headingDelta = 15, view = false },
+    marker = {
+      x = -660.676330,
+      y = -854.488159,
+      z = 41.430309 + 2,
+      scale = 0.5,
       bopUpAndDown = true,
       color = { r = 0, g = 170, b = 0, a = 192 }
     },
@@ -93,22 +233,23 @@ end
 
 Citizen.CreateThread(function()
   for _, info in pairs(blips) do
-    info.blip = AddBlipForCoord(info.x, info.y, info.z)
-    SetBlipSprite(info.blip, info.id)
-    SetBlipDisplay(info.blip, 4)
-    SetBlipScale(info.blip, 1.0)
-    SetBlipColour(info.blip, info.colour)
-    SetBlipAsShortRange(info.blip, true)
-    BeginTextCommandSetBlipName("STRING")
-    AddTextComponentSubstringKeyboardDisplay(info.title)
-    EndTextCommandSetBlipName(info.blip)
+    if (not info.hidden) then
+      info.blip = AddBlipForCoord(info.x, info.y, info.z)
+      SetBlipSprite(info.blip, info.id)
+      SetBlipDisplay(info.blip, 4)
+      SetBlipScale(info.blip, 1.0)
+      SetBlipColour(info.blip, info.colour)
+      SetBlipAsShortRange(info.blip, true)
+      BeginTextCommandSetBlipName("STRING")
+      AddTextComponentSubstringKeyboardDisplay(info.title)
+      EndTextCommandSetBlipName(info.blip)
+    end
   end
   Citizen.CreateThread(function()
     while not ESX.IsPlayerLoaded() do
       Citizen.Wait(1000)
     end
     local debugUIWasVisible = Config.debug.uiVisible
-    local shapeTestResult = 2
     while true do
       Citizen.Wait(0)
       ESX.GetPlayerData()
@@ -118,7 +259,6 @@ Citizen.CreateThread(function()
           GetLocalTime()
       local playerHeading = GetEntityHeading(xPlayer.ped)
       local playerCoords = GetEntityCoords(xPlayer.ped, false)
-      local shapeTestHandle = -1
       local debugInfo = {
         blipIndex = -1,
         blipInfo = {},
@@ -137,6 +277,11 @@ Citizen.CreateThread(function()
       }
       for atmId, info in pairs(blips) do
         local marker = info.marker
+        local distance = GetDistanceBetweenCoords(playerCoords.x, playerCoords.y, playerCoords.z, info.x, info.y, info.z,
+          true)
+        local headingDelta = math.abs(info.interactVec.heading - playerHeading)
+        local isFacingATM = headingDelta < info.interactVec.headingDelta
+        local isCloseForInteraction = distance < info.interactVec.distance
         DrawMarker(
           29,
           marker.x, marker.y, marker.z,
@@ -146,21 +291,16 @@ Citizen.CreateThread(function()
           marker.color.r, marker.color.g, marker.color.b, marker.color.a,
           marker.bopUpAndDown, true, 2, false, nil, nil, false
         )
-        local distance = GetDistanceBetweenCoords(playerCoords.x, playerCoords.y, playerCoords.z, info.x, info.y, info.z,
-          true)
-        local headingDelta = math.abs(info.interactVec.heading - playerHeading)
-        local isFacingATM = headingDelta < info.interactVec.headingDelta
-        local isCloseForInteraction = distance < info.interactVec.distance
-        if ((not info.shapeTestResult == 1 or not info.shapeTestHandle) and isCloseForInteraction) then
-          info.shapeTestHandle = StartShapeTestLosProbe(playerCoords.x, playerCoords.y, playerCoords.z,
-            marker.x, marker.y,
-            playerCoords.z, 16, 0, 7)
-        end
-        info.shapeTestResult, info.shapeTestHit, info.shapeTestEndCoords, info.shapeTestSurfaceNormal, info.shapeTestEntityHit =
-            GetShapeTestResult(info.shapeTestHandle)
-        if (info.shapeTestResult == 2) then
-          info.shapeTestModel = GetEntityModel(info.shapeTestEntityHit)
-          print('atm %s shap test finished! Model: %s', atmId, info.shapeTestModel)
+        if (info.interactVec.view) then
+          DrawMarker(
+            26,
+            info.x, info.y, info.z,
+            0.0, 1.0, 0.0,
+            0.0, 0.0, 0.0,
+            marker.scale, marker.scale, marker.scale,
+            marker.color.r, marker.color.g, marker.color.b, marker.color.a,
+            false, false, 2, false, nil, nil, false
+          )
         end
         if (isCloseForInteraction and isFacingATM) then
           DisplayInteractionText(_U('interactKeyMapMsg', Config.InteractKeyMap.keyboard.text), info.marker)
@@ -181,15 +321,6 @@ Citizen.CreateThread(function()
             debugInfo.isFacingATM = isFacingATM
             debugInfo.isCloseForInteraction = isCloseForInteraction
             debugInfo.canInteract = canInteract
-            if (info.shapeTestResult == 2 and isCloseForInteraction) then
-              -- print('shape test ok, model:', isAtmModel(GetEntityModel(info.shapeTestEntityHit)))
-              debugInfo.shapeTestResult.hit = info.shapeTestHit
-              debugInfo.shapeTestResult.endCoords = info.shapeTestEndCoords
-              debugInfo.shapeTestResult.surfaceNormal = info.shapeTestSurfaceNormal
-              debugInfo.shapeTestResult.entityHit = info.shapeTestEntityHit
-              debugInfo.shapeTestResult.model = GetEntityModel(info.shapeTestEntityHit)
-              --   print(json.encode(debugInfo.shapeTestResult))
-            end
           end
         end
         debugUIWasVisible = Config.debug.uiVisible
